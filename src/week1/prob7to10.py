82b904636330847b595b60f3c519b9ff8ce5d129
Ken Zhu <ken_zhu@outlook.com.au>
2017-09-29 14:15:27 +1000

    Install python packages

--- src/week1/prob7to10.py
+++ src/week1/prob7to10.py
@@ -2,6 +2,11 @@
 Created on 29/09/2017
 
 @author: ken_zhu
+
+Visit this link (http://www.lfd.uci.edu/~gohlke/pythonlibs/) if you need Python packages on Windows.
+pip install wheel from Script folder of Python installation
+pip install *.whl
+
 '''
 
 if __name__ == '__main__':
